43cb78461328418c2d682527dfb771971aac04c6
alexsousarodrigues1919-max <alexsousarodrigues1919@gmail.com>
2026-04-20 19:02:34 -0300

    enviar atividade

---
+++ js/aula15/atividade01.js
@@ -0,0 +1,21 @@
+//crie um objeto que represente um usuário com : nome, email, cargo, idade, senha e telefone
+//essas informações deverão ser solicitadas ao usuário e armazenadas no objeto
+//ao final mostre cada uma das informações armazenadas no objeto no console no seguinte formato
+
+//nome: valor
+//email: valor
+//cargo: valor
+//idade: valor
+//senha: valor
+//telefone: valor
+
+let usuario ={
+    nome: 'Alex',
+    email: 'alex@gamil.com',
+    cargo: 'full Stack',
+    idade: 20,
+    senha: 'Alex123',
+    telefone: 99984686139
+}
+
+console.log(usuario);
---
+++ js/aula15/script.js
@@ -0,0 +1,4 @@
+// arrays comparadas com objetos
+// uma lista de numeros (todos as infomação)
+
+let numeros = [1, 2, 3]
