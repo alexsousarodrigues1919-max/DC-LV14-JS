a0b96f49eb2c0768017d19a48ce446d015031527
alexsousarodrigues1919-max <alexsousarodrigues1919@gmail.com>
2026-05-18 21:41:53 -0300

    enviar atividades

--- react/aula23/src/App.jsx
+++ react/aula23/src/App.jsx
@@ -1,19 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-import MeuPrimeiroComponente from './components/MeuPrimeiroComponente'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import MeuPrimeiroComponente from "./components/MeuPrimeiroComponente";
+import InfoUsuario from "./components/InfoUsuario";
+import InfoUsuarioProps from "./components/InfoUsuarioProps";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <MeuPrimeiroComponente />
       <h1>Vite + React</h1>
+      <div id="Atividade01">
+        <InfoUsuario />
+      </div>
+      <div id="Atividade02">
+        <InfoUsuarioProps
+          nome="João"
+          idade="30"
+          profissao="Engenheiro de Software"
+          email="5KXfH@example.com"
+        />
+        <InfoUsuarioProps
+          nome="Maria"
+          idade="25"
+          profissao="Desenvolvedora Front-end"
+          email="VZ2Jv@example.com"
+        />
+        <InfoUsuarioProps
+          nome="Pedro"
+          idade="28"
+          profissao="Analista de Dados"
+          email="tX4oH@example.com"
+        />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ react/aula23/src/components/InfoUsuario.jsx
@@ -0,0 +1,10 @@
+export default function infoUsuario() {
+    return (
+        <div>
+            <h1>Nome: João</h1>
+            <h2>Idade: 30</h2>
+            <h3>Profissão: Engenheiro de Software</h3>
+            <h4>Email: 5KXfH@example.com</h4>
+        </div>
+    );
+}
